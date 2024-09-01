9b118c481b7e99faa4fe33e188649f3dca421a79
seiniabaya <seiniabaya@gmail.com>
2024-09-01 03:00:27 +0100

    initial commit: project initialisation, installation of tailwindcss for styling

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold underline">
+        Inventory Management App
+      </h1>
+    </div>
+  )
+}
+
+export default App
